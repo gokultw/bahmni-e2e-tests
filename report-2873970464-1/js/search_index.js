5878867eb9506dc123012a18ba2b807d9f660965
gokultw <gokul.kuppan@thoughtworks.com>
2022-08-17 14:19:54 +0530

    Deploy Reports

---
+++ report-2873970464-1/js/search_index.js
@@ -0,0 +1,1 @@
+var index = {"Tags":{"clinic":["specs/zlabLite.html","specs/paymentLite.html"],"lab-lite":["specs/zlabLite.html"],"payment-lite":["specs/paymentLite.html"]},"Specs":{"Lab Lite":["specs/zlabLite.html"],"Payment Lite":["specs/paymentLite.html"]}};
