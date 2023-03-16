917c7c4c0fa8c04366f928c14c5cca4aa838ff15
Gokul Kuppan <104197616+gokultw@users.noreply.github.com>
2023-03-16 11:25:11 +0530

    Update waitForEnv.sh

--- .github/waitForEnv.sh
+++ .github/waitForEnv.sh
@@ -19,6 +19,7 @@ do
                 echo $url is not reachable with status code $statusCode, retrying after $retry_time seconds, remaining time left $max_timeout seconds
                 sleep $retry_time
                 max_timeout=`expr $max_timeout - $retry_time`
+                break
         else
                 flag=true
                 echo "ENV_FLAG=true" >> $GITHUB_ENV
